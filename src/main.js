6f43c5c43580653b96f321a1e62bd264a8f7ed5e
DivineBlow <sebi.26.03.97@mail.ru>
2023-05-07 15:09:30 +0300

    Initialize new project!

---
+++ src/main.js
@@ -0,0 +1,70 @@
+import { Telegraf, session } from 'telegraf'
+import { message } from 'telegraf/filters'
+import { code } from 'telegraf/format'
+import config from 'config'
+import { audio } from './audio.js'
+import { openai } from './openAI.js'
+
+const INITIAL_SESSION = {
+  messages: []
+}
+
+const bot = new Telegraf(config.get('TG_TOKEN'))
+
+bot.use(session())
+
+bot.command('new', async (ctx) => {
+  ctx.session = INITIAL_SESSION
+  await ctx.reply('Жду вашего голосового или текстового сообщения')
+})
+
+bot.command('start', async (ctx) => {
+  ctx.session = INITIAL_SESSION
+  await ctx.reply('Жду вашего голосового или текстового сообщения')
+})
+
+bot.on(message('voice'), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION
+  try {
+    await ctx.reply(code('Подождите секундочку, обрабатываю запрос...'))
+
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+    const userID = String(ctx.message.from.id)
+
+    const audioPath = await audio.create(link.href, userID)
+    const mp3Path = await audio.toMP3(audioPath, userID)
+
+    const text = await openai.transcription(mp3Path)
+    await ctx.reply(code(`Ваш запрос: ${text}`))
+
+    ctx.session.messages.push({role: openai.roles.USER, content: text})
+    const response = await openai.chat(ctx.session.messages)
+    ctx.session.messages.push({role: openai.roles.ASSISTANT, content: response.content})
+    
+    await ctx.reply(response.content)
+
+  } catch (e) {
+    console.log(`Error while voice message`, e.message)
+  }
+})
+
+bot.on(message('text'), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION
+  try {
+    await ctx.reply(code('Подождите секундочку, обрабатываю запрос...'))
+
+    ctx.session.messages.push({role: openai.roles.USER, content: ctx.message.text})
+    const response = await openai.chat(ctx.session.messages)
+    ctx.session.messages.push({role: openai.roles.ASSISTANT, content: response.content})
+    
+    await ctx.reply(response.content)
+
+  } catch (e) {
+    console.log(`Error while voice message`, e.message)
+  }
+})
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
